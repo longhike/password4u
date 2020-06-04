77fc4a754509c1c37323c7f5066426d0bc5de049
cheese stick <61661631+longhike@users.noreply.github.com>
2020-06-03 22:16:15 -0400

    html form ready, first js variables in place, first console.log ready

---
+++ script.js
@@ -0,0 +1,10 @@
+// These are the global variables with the different passcode parameters
+var choiceLtr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var choiceNum = "0123456789";
+var choiceLtrNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+var choiceLtrNumChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@#$%^&_";
+// This is the variable containing the input for the number of characters section
+var numChars = "" // to be defined
+
+console.log(choiceLtrNumChar.split(""));
+

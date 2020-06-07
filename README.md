5f5646b525f6c7fe1b53e371fe12a0b22b9d9574
cheese stick <61661631+longhike@users.noreply.github.com>
2020-06-06 22:22:57 -0400

    still need to add rest of readme. javascript doesn't alert properly, pseudocode needed.

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # hwThree
+
+### Created on 6/2/2020
+
+### By Sam Ascheim
+
+## Description:
+
+This is my first pass at a password generator, in which the user enters the number characters and their parameters and, when the "Generate Password" button is clicked, their generated password is alerted to the browser.
+
+## Set-up & Installation requirements:
+
+None needed besides an up-to-date web browser.
+
+## Known Bugs:
+
+1. Can't figure out how to actually alert the password. I'm having trouble
+
+## Support & Contact details:
+
+e-mail sam.ascheim@gmail.com
+
+## Technologies Used
+
+HTML
+
+CSS
+
+Boostrap
+
+Javascript
+
+## Contribution Guidlines 
+​
+Direct link to site:
+https://longhike.github.io/hwTwo/
+​
+### License
+​
+Copyright(c) 2020 By Sam Ascheim
+​
+This software is licensed under MIT license.
--- script.js
+++ script.js
@@ -1,10 +1,57 @@
-// These are the global variables with the different passcode parameters
+// This is the global variable set with the different word parameters
 var choiceLtr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var choiceNum = "0123456789";
 var choiceLtrNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 var choiceLtrNumChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@#$%^&_";
-// This is the variable containing the input for the number of characters section
-var numChars = "" // to be defined
+// This is the global variable set containing the input for the number of characters and the submit button
+var numChars = document.querySelector("#passLength");
+var submitSam = document.querySelector("#submitSam");
+// This is the global variable containing the parameter drop-down
+var paramSamMain = document.querySelector("#selectSam"); 
 
-console.log(choiceLtrNumChar.split(""));
+submitSam.addEventListener("click", function() {
+  // to get number of characters
+  var getChars = parseInt(numChars.value);
+    console.log(typeof getChars)
+  // to get parameter selection
+  var getParam = paramSamMain.options[paramSamMain.selectedIndex].value;
+    console.log(getParam);
+  // if function to get the proper alert given parameter selection
+  // if selection is letters only:
+  if (getParam.value = "dropLtr") {
+    // take the string of all letter options and splits it into an array
+    var splitLtr = choiceLtr.split("");
+      console.log(splitLtr);
+      var ltrPass = splitLtr[Math.floor(Math.random()) * splitLtr.length];
+      alert(ltrPass.toString);
 
+   }
+    
+  // if selection is numbers only:
+  else if (getParam.value = "dropNum") {
+
+  }
+  // if selection is letters and numbers:
+  else if (getParam.value = "dropLtrNum") {
+
+  }
+  // if the selection is letters, numbers and 
+  else if (gegParam.value = "dropLtrNumChar"){
+
+  }
+
+  // alert("omg, the button works!");
+});
+
+// ex function to substring
+
+// function myFunction() {
+//     var str = "Hello world!";
+//     var res = str.substring(0, 10);
+//     document.getElementById("demo").innerHTML = res;
+//   }
+
+
+// console.log(choiceLtrNumChar.split(""));
+// console.log(typeof numChars);
+ 
